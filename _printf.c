3aba32a8dd7e2ae00dfcca759a3aa96c9c2ad624
Didizakaria <dev.didizakaria@gmail.com>
2023-04-27 12:06:44 -0700

    0

--- _printf.c
+++ _printf.c
@@ -1,132 +1,67 @@
 #include "main.h"
 
-/**
- * skip_flags - skip flags after '%'.
- * @format: the format.
- * @index: index in format.
- * @flags: struct that contains the flags to use
- * Return: new index after skipping flags.
-*/
-
-int	skip_flags(const char *format, int index, flags_t *flags)
-{
-	while (format[index] == ' ' || format[index] == '+'
-			|| format[index] == '#')
-	{
-		if (format[index] == ' ')
-			flags->space = 1;
-		if (format[index] == '+')
-			flags->plus = 1;
-		if (format[index] == '#')
-			flags->hash = 1;
-		index++;
-	}
-	while (format[index] == 'l' || format[index] == 'h')
-	{
-		if (format[index] == 'l')
-			flags->l++;
-		if (format[index] == 'h')
-			flags->h++;
-		index++;
-	}
-	return (index);
-}
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * check_format - checks for valid formats.
- * @c: format character.
- * Return: 1 if it's valid, otherwise 0.
-*/
-int check_format(char c)
-{
-	if (c == 'c' || c == 's' || c == '%' || c == 'X' || c == 'o'
-		|| c == 'd' || c == 'b' || c == 'i' || c == 'x' ||
-		c == 'u' || c == 'S' || c == 'p')
-		return (1);
-	return (0);
-}
-
-/**
- * print_else - prints the rest of the string
- * if no conversion specifier is found
- * @format: the format string to use
- * @i: the index of the format string
- * @tmp: the index of the format string
- * Return: the number of characters printed
-*/
-int print_else(const char *format, int i, int tmp)
-{
-	int count = 0, l = 0, h = 0;
-
-	if (!format[i])
-		return (-1);
-	while (tmp <= i)
-	{
-		if (format[tmp] == 'l' && l == 0)
-		{
-			l = 1;
-			if (format[tmp + 1] == 'l')
-				tmp++;
-			tmp++;
-		}
-		else if (format[tmp] == 'h' && h == 0)
-		{
-			h = 1;
-			if (format[tmp + 1] == 'h')
-				tmp++;
-			tmp++;
-		}
-		count += _putchar(format[tmp++]);
-	}
-	return (count);
-}
-/**
- * _printf - prints formatted output to stdout
- * @format: the format string to use
- *
- * Return: the number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	flags_t flags = {0, 0, 0, 0, 0};
-	int i = 0;
-	int tmp;
-	int count = 0;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
 
 	va_start(list, format);
-	if (!format)
-		return (-1);
-	while (format && format[i])
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			tmp = i;
-			i = skip_flags(format, i + 1, &flags);
-			if (!format[i])
-				return (-1);
-			if ((check_format(format[i])) && ((flags.l <= 2 && !flags.h)
-				|| (flags.h <= 2 && !flags.l)))
-			{
-				count += print_content(list, format[i], flags);
-				flags.plus = 0;
-				flags.space = 0;
-				flags.hash = 0;
-				flags.l = 0;
-				flags.h = 0;
-			}
-			else
-			{
-				flags.l = 0;
-				flags.h = 0;
-				count += print_else(format, i, tmp);
-			}
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
-			count += _putchar(format[i]);
-		i++;
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
+
+	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
-	return (count);
+
+	return (printed_chars);
 }
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
