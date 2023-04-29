dd8990ab25b0d364ca0302d95653f0d82cf2c49f
Didizakaria <dev.didizakaria@gmail.com>
2023-04-28 17:47:23 -0700

    0

--- _printf.c
+++ _printf.c
@@ -1,71 +1,49 @@
 #include "main.h"
 
 /**
- * check_sp - check which specifier to print
- * @format: string passed
- * @print_arr: array of struct
- * @list: arguments
- * Return: numb of char to be printed
- */
-int check_sp(const char *format, fmt_t *print_arr, va_list list)
-{
-	int count = 0, i = 0, j = 0;
-
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			j = 0;
-			i++;
-			while (print_arr[j].type != NULL &&
-			       format[i] != *(print_arr[j].type))
-				j++;
-			if (print_arr[j].type != NULL)
-				count = count + print_arr[j].f(list);
-			else
-			{
-				if (format[i] == '\0')
-					return (-1);
-				if (format[i] != '%')
-					count += _putchar('%');
-				count += _putchar(format[i]);
-			}
-		}
-		else
-			count += _putchar(format[i]);
-		i++;
-	}
-	return (count);
-}
-
-/**
- * _printf - prints according to format
- * @format: string passed
- * Return: char to be printed
+ * _printf - prints anything
+ * @format: the format string
+ *
+ * Return: number of bytes printed
  */
 int _printf(const char *format, ...)
 {
-	va_list list;
-	int a = 0;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-	fmt_t ops[] = {
-		{"c", ch},
-		{"s", str},
-		{"d", _int},
-		{"b", _bin},
-		{"i", _int},
-		{"u", _ui},
-		{"o", _oct},
-		{"x", _hex_l},
-		{"X", _hex_u},
-		{"R", _rot13},
-		{NULL, NULL}
-	};
+	va_start(ap, format);
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(list, format);
-	a = check_sp(format, ops, list);
-	va_end(list);
-	return (a);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')
+		{
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
+		{
+			p++; /* next char */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
